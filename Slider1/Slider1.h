e10d7048351afb9d96b3143be4c858a780a7014a
Max <mbreges@gmail.com>
2025-11-14 20:49:03 +0300

    Slider1

---
+++ Slider1/Slider1.h
@@ -0,0 +1,25 @@
+#ifndef SLIDER1_H_INCLUDED
+#define SLIDER1_H_INCLUDED
+
+#include "../se_sdk3/mp_sdk_audio.h"
+#include "../se_sdk3/smart_audio_pin.h"
+
+class Slider : public MpBase2
+{
+public:
+	Slider();
+	int32_t MP_STDCALL open()
+	{
+		pinValueOut.setCurveType(SmartAudioPin::LinearAdaptive); // Automatic.
+		return MpBase2::open();
+	}
+	void subProcess(int sampleFrames);
+	void onSetPins() override;
+
+private:
+	FloatInPin pinValueIn;
+	SmartAudioPin pinValueOut;
+};
+
+#endif
+
