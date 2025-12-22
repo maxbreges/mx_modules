ea2caffb89eae451797bf20b4e005a707fe7e37e
Max <mbreges@gmail.com>
2025-12-23 00:50:08 +0300

    Update ScaleDegreeGui.cpp

--- ScaleDegree/ScaleDegreeGui.cpp
+++ ScaleDegree/ScaleDegreeGui.cpp
@@ -25,7 +25,7 @@ class MajorsAscGui final : public SeGuiInvisibleBase
 			switch (root)
 			{
 			case 0: pinKeyList = "C,D♭,D,E♭,E,F,F♯,G,A♭,A,B♭,B"; break;
-			case 1: pinKeyList = L"C♯,D♮,D♯,E♮,E♯,F♯,F𝄪,G♯,A♮,A♯,B♮,B♯"; break;
+			case 1: pinKeyList = "C♯,D♮,D♯,E♮,E♯,F♯,F♯♯,G♯,A♮,A♯,B♮,B♯"; break;
 			case 2: pinKeyList = "D,E♭,E,F♮,F♯,G,G♯,A,B♭,B,C♮,C♯"; break;
 			case 3: pinKeyList = "E♭,F♭,F,G♭,G,A♭,A♮,B♭,C♭,C,D♭,D"; break;
 			case 4: pinKeyList = "E,F♮,F♯,G♮,G♯,A,A♯,B,C♮,C♯,D♮,D♯"; break;
@@ -44,17 +44,17 @@ class MajorsAscGui final : public SeGuiInvisibleBase
 			switch (root)
 			{
 			case 0: pinKeyList = "C,C♯,D,D♯,E,F,F♯,G,G♯,A,B♭,B"; break;
-			case 1: pinKeyList = L"C♯,C𝄪,D♯,D𝄪,E♯,F♯,F𝄪,G♯,G𝄪,A♯,B♮,B♯"; break;
+			case 1: pinKeyList = "C♯,C♯♯,D♯,D♯♯,E♯,F♯,F♯♯,G♯,G♯♯,A♯,B♮,B♯"; break;
 			case 2: pinKeyList = "D,D♯,E,E♯,F♯,G,G♯,A,A♯,B,C♮,C♯"; break;
 			case 3: pinKeyList = "E♭,E♮,F,F♯,G,A♭,A♮,B♭,B♮,C,D♭,D"; break;
-			case 4: pinKeyList = L"E,E♯,F♯,F𝄪,G♯,A,A♯,B,B♯,C♯,D♮,D♯"; break;
+			case 4: pinKeyList = "E,E♯,F♯,F♯♯,G♯,A,A♯,B,B♯,C♯,D♮,D♯"; break;
 			case 5: pinKeyList = "F,F♯,G,G♯,A,B♭,B♮,C,C♯,D,E♭,E"; break;
-			case 6: pinKeyList = L"F♯,F𝄪,G♯,G𝄪,A♯,B,B♯,C♯,C𝄪,D♯,E♮,E♯"; break;
+			case 6: pinKeyList = "F♯,F♯♯,G♯,G♯♯,A♯,B,B♯,C♯,C♯♯,D♯,E♮,E♯"; break;
 			case 7: pinKeyList = "G,G♯,A,A♯,B,C,C♯,D,D♯,E,F♮,F♯"; break;
 			case 8: pinKeyList = "A♭,A♮,B♭,B♮,C,D♭,D♮,E♭,E♮,F,G♭,G"; break;
 			case 9: pinKeyList = "A,A♯,B,B♯,C♯,D,D♯,E,E♯,F♯,G♮,G♯"; break;
 			case 10: pinKeyList = "B♭,B♮,C,C♯,D,E♭,E♮,F,F♯,G,A♭,A"; break;
-			case 11: pinKeyList = L"B,B♯,C♯,C𝄪,D♯,E,E♯,F♯,F𝄪,G♯,A♮,A♯"; break;
+			case 11: pinKeyList = "B,B♯,C♯,C♯♯,D♯,E,E♯,F♯,F♯♯,G♯,A♮,A♯"; break;
 			}
 			pinDegreeList = "Ⅰ, Ⅰ♯, Ⅱ, Ⅱ♯, Ⅲ, Ⅳ, Ⅳ♯, Ⅴ, Ⅴ♯, Ⅵ, Ⅶ♭, Ⅶ"; break;			
 		}
@@ -63,14 +63,14 @@ class MajorsAscGui final : public SeGuiInvisibleBase
 			switch (root)
 			{
 			case 0: pinKeyList = "C,D♭,D,E♭,E♮,F,F♯,G,A♭,A♮,B♭,B♮"; break;
-			case 1: pinKeyList = L"C♯,D♮,D♯,E,E♯,F♯,F𝄪,G♯,A,A♯,B,B♯"; break;
+			case 1: pinKeyList = "C♯,D♮,D♯,E,E♯,F♯,F♯♯,G♯,A,A♯,B,B♯"; break;
 			case 2: pinKeyList = "D,E♭,E,F,F♯,G,G♯,A,B♭,B♮,C,C♯"; break;
 			case 3: pinKeyList = "E♭,F♭,F,G♭,G♮,A♭,A♮,B♭,C♭,C♮,D♭,D♮"; break;
 			case 4: pinKeyList = "E,F♮,F♯,G,G♯,A,A♯,B,C,C♯,D,D♯"; break;
 			case 5: pinKeyList = "F,G♭,G,A♭,A♮,B♭,B♮,C,D♭,D♮,E♭,E♮"; break;
 			case 6: pinKeyList = "F♯,G♮,G♯,A,A♯,B,B♯,C♯,D,D♯,E,E♯"; break;
 			case 7: pinKeyList = "G,A♭,A,B♭,B♮,C,C♯,D,E♭,E♮,F,F♯"; break;
-			case 8: pinKeyList = L"G♯,A♮,A♯,B,B♯,C♯,C𝄪,D♯,E,E♯,F♯,F𝄪"; break;
+			case 8: pinKeyList = "G♯,A♮,A♯,B,B♯,C♯,C♯♯,D♯,E,E♯,F♯,F♯♯"; break;
 			case 9: pinKeyList = "A,B♭,B,C,C♯,D,D♯,E,F,F♯,G,G♯"; break;
 			case 10: pinKeyList = "B♭,C♭,C,D♭,D♮,E♭,E♮,F,G♭,G♮,A♭,A♮"; break;
 			case 11: pinKeyList = "B,C♮,C♯,D,D♯,E,E♯,F♯,G,G♯,A,A♯"; break;
